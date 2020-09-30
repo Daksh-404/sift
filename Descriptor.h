cc343744e74ca581868ba4959aff1315c4fb41cd
Daksh Gupta <61038855+Daksh-404@users.noreply.github.com>
2020-09-30 23:41:47 +0530

    Update Descriptor.h

--- Descriptor.h
+++ Descriptor.h
@@ -7,22 +7,19 @@
  * this code :)
  ******************************************************/
 
+#pragma once
 #ifndef DESCRIPTOR_H
 #define DESCRIPTOR_H
-
+#include<vector>
 using namespace std;
 
 class Descriptor
 {
 public:
 	float			xi, yi;		// The location
-	vector<double>	fv;			// The feature vector
+	vector<float>	fv;			// The feature vector
 
-	Descriptor()
-	{
-	}
-
-	Descriptor(float x, float y, vector<double> const& f)
+	Descriptor(float x, float y, vector<float> const& f)
 	{
 		xi = x;
 		yi = y;
